73dc948d1ebdf478eff004ec0725ce2991a9e2cc
ErykMajoch <eryk.majoch13@gmail.com>
2023-10-26 23:41:02 +0100

    Created a super basic test package to check if linking works correct

--- test_package/src/example.cpp
+++ test_package/src/example.cpp
@@ -5,14 +5,7 @@
 
 int main() {
     using namespace meltrix;
-    Matrix m = Matrix(4, 3, {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12});
-
-    std::cout << "Current matrix:\n";
-    m.print();
-
-    Matrix m2 = m.getSubMatrix(1, 2);
-    std::cout << "\nSub matrix from [1,2]:\n";
-    m2.print();
-
+    Matrix m = Matrix(2, 2, {1, 2, 3, 4});
+    std::cout << "Matrix successfully created!\n";
     return 0;
 }
